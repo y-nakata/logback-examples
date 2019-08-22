5bf0a3e9d91788500d1321c6fb50d23b162547b0
y-nakata <yukinorinkt@gmail.com>
2019-08-23 00:17:58 +0900

    introduction done

---
+++ src/main/java/chapters/introduction/HelloWorld1.java
@@ -0,0 +1,13 @@
+package chapters.introduction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+
+		Logger logger = LoggerFactory.getLogger("chapters.introduction.HelloWorld1");
+		logger.debug("Hello World.");
+	}
+}
